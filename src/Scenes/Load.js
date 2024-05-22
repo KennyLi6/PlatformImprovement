44380d1a153bb14423d70e9a9cf04bab94a74dcd
KennyLi6 <kennyli3219@gmail.com>
2024-05-21 20:49:26 -0700

    Revert "Game 3 upload"
    This reverts commit 0c1d1dbfb6a47180d18a0e8ed4296710486365af.

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -7,11 +7,11 @@ class Load extends Phaser.Scene {
         this.load.setPath("./assets/");
 
         // Load characters spritesheet
-        this.load.atlas("platformer_characters", "platformerPack_character.png", "tilemap-characters-packed.json");
+        this.load.atlas("platformer_characters", "tilemap-characters-packed.png", "tilemap-characters-packed.json");
 
         // Load tilemap information
         this.load.image("tilemap_tiles", "tilemap_packed.png");                         // Packed tilemap
-        this.load.tilemapTiledJSON("platformer-level-1", "game-3-draft.tmj");   // Tilemap in JSON
+        this.load.tilemapTiledJSON("platformer-level-1", "platformer-level-1.tmj");   // Tilemap in JSON
 
         // Load the tilemap as a spritesheet
         this.load.spritesheet("tilemap_sheet", "tilemap_packed.png", {
@@ -19,22 +19,13 @@ class Load extends Phaser.Scene {
             frameHeight: 18
         });
 
-        this.load.spritesheet("collectable_sheet", "tile_0107.png", {
-            frameWidth: 18,
-            frameHeight: 18
-        });
-
-        this.load.audio("jump", "jingles_PIZZI16.ogg");
-        this.load.audio("land", "footstep_snow_004.ogg");
-        this.load.audio("pickup", "click_002.ogg");
-
         // Oooh, fancy. A multi atlas is a texture atlas which has the textures spread
         // across multiple png files, so as to keep their size small for use with
         // lower resource devices (like mobile phones).
         // kenny-particles.json internally has a list of the png files
         // The multiatlas was created using TexturePacker and the Kenny
         // Particle Pack asset pack.
-        this.load.image("kenny-particles", "tile_0122.png");
+        this.load.multiatlas("kenny-particles", "kenny-particles.json");
     }
 
     create() {
@@ -42,8 +33,8 @@ class Load extends Phaser.Scene {
             key: 'walk',
             frames: this.anims.generateFrameNames('platformer_characters', {
                 prefix: "tile_",
-                start: 1,
-                end: 2,
+                start: 0,
+                end: 1,
                 suffix: ".png",
                 zeroPad: 4
             }),
@@ -64,7 +55,7 @@ class Load extends Phaser.Scene {
             key: 'jump',
             defaultTextureKey: "platformer_characters",
             frames: [
-                { frame: "tile_0003.png" }
+                { frame: "tile_0001.png" }
             ],
         });
 
--- src/Scenes/Platformer.js
+++ src/Scenes/Platformer.js
@@ -1,5 +1,3 @@
-const tilesize = 18;
-
 class Platformer extends Phaser.Scene {
     constructor() {
         super("platformerScene");
@@ -7,29 +5,23 @@ class Platformer extends Phaser.Scene {
 
     init() {
         // variables and settings
-        this.ACCELERATION = 20*tilesize;
-        this.DRAG = 15*tilesize;    // DRAG < ACCELERATION = icy slide
-        this.physics.world.gravity.y = 600;
-        this.JUMP_VELOCITY = -tilesize*18;
+        this.ACCELERATION = 400;
+        this.DRAG = 500;    // DRAG < ACCELERATION = icy slide
+        this.physics.world.gravity.y = 1500;
+        this.JUMP_VELOCITY = -600;
         this.PARTICLE_VELOCITY = 50;
         this.SCALE = 2.0;
-        this.MAX_SPEED = 6*tilesize;
-        this.gameOver = false;
-        this.score = 0;
-        this.displayed = false;
-        this.airborne = false;
-        this.timer = 2;
     }
 
     create() {
         // Create a new tilemap game object which uses 18x18 pixel tiles, and is
         // 45 tiles wide and 25 tiles tall.
-        this.map = this.add.tilemap("platformer-level-1", tilesize, tilesize, 90, 30);
+        this.map = this.add.tilemap("platformer-level-1", 18, 18, 45, 25);
 
         // Add a tileset to the map
         // First parameter: name we gave the tileset in Tiled
         // Second parameter: key for the tilesheet (from this.load.image in Load.js)
-        this.tileset = this.map.addTilesetImage("tilemap_packed", "tilemap_tiles");
+        this.tileset = this.map.addTilesetImage("kenny_tilemap_packed", "tilemap_tiles");
 
         // Create a layer
         this.groundLayer = this.map.createLayer("Ground-n-Platforms", this.tileset, 0, 0);
@@ -49,8 +41,8 @@ class Platformer extends Phaser.Scene {
 
         this.coins = this.map.createFromObjects("Objects", {
             name: "coin",
-            key: "collectable_sheet",
-            frame: 0
+            key: "tilemap_sheet",
+            frame: 151
         });
 
         this.spawn = this.map.createFromObjects("Objects", {
@@ -59,27 +51,26 @@ class Platformer extends Phaser.Scene {
             frame: 145
         });
 
-        this.flag = this.map.createFromObjects("Objects", {
-            name: "flag",
+        this.powerup = this.map.createFromObjects("Objects", {
+            name: "powerup",
             key: "tilemap_sheet",
-            frame: 131
+            frame: 67
         })
 
         // Since createFromObjects returns an array of regular Sprites, we need to convert 
         // them into Arcade Physics sprites (STATIC_BODY, so they don't move) 
         this.physics.world.enable(this.coins, Phaser.Physics.Arcade.STATIC_BODY);
 
-        this.physics.world.enable(this.flag, Phaser.Physics.Arcade.STATIC_BODY);
+        this.physics.world.enable(this.powerup, Phaser.Physics.Arcade.STATIC_BODY);
 
         // Create a Phaser group out of the array this.coins
         // This will be used for collision detection below.
         this.coinGroup = this.add.group(this.coins);
-
-        this.flagGroup = this.add.group(this.flag);
+        
+        this.powerGroup = this.add.group(this.powerup);
 
         // set up player avatar
         my.sprite.player = this.physics.add.sprite(this.spawn[0].x, this.spawn[0].y, "platformer_characters", "tile_0000.png");
-        this.physics.world.setBounds(0,0,tilesize*90,tilesize*32);
         my.sprite.player.setCollideWorldBounds(true);
 
         // Enable collision handling
@@ -88,14 +79,11 @@ class Platformer extends Phaser.Scene {
         // Handle collision detection with coins
         this.physics.add.overlap(my.sprite.player, this.coinGroup, (obj1, obj2) => {
             obj2.destroy(); // remove coin on overlap
-            this.score += 100;
-            this.sound.play("pickup", {
-                volume: 1
-            });
         });
-
-        this.physics.add.overlap(my.sprite.player, this.flagGroup, () => {
-            this.gameOver = true;
+        
+        this.physics.add.overlap(my.sprite.player, this.powerGroup, (obj1, obj2) => {
+            obj2.destroy();
+            this.JUMP_VELOCITY = -1000;
         })
 
         // set up Phaser-provided cursor key input
@@ -110,15 +98,16 @@ class Platformer extends Phaser.Scene {
         }, this);
 
         // Add movement vfx here
-        my.vfx.walking = this.add.particles(0, 6, "kenny-particles", {
+        my.vfx.walking = this.add.particles(0, 0, "kenny-particles", {
+            frame: ['smoke_03.png', 'smoke_09.png'],
             // Try: add random: true
-            scale: {start: 0.1, end: 0.1, random: true},
+            scale: {start: 0.03, end: 0.1, random: true},
             // Try: maxAliveParticles: 8,
             lifespan: 350,
             maxAliveParticles: 8,
             // Try: gravityY: -400,
             alpha: {start: 1, end: 0.1}, 
-            gravityY: -400
+            gravityY: -400,
         });
 
         my.vfx.walking.stop();
@@ -126,19 +115,15 @@ class Platformer extends Phaser.Scene {
         // camera code here
         this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
         this.cameras.main.startFollow(my.sprite.player, true, 0.25, 0.25); // (target, [,roundPixels][,lerpX][,lerpY])
-        this.cameras.main.setDeadzone(0, 5);
+        this.cameras.main.setDeadzone(50, 50);
         this.cameras.main.setZoom(this.SCALE);
-        this.physics.world.drawDebug = false;
+
     }
 
     update() {
-        if(!this.gameOver && cursors.left.isDown) {
-            if (my.sprite.player.body.velocity.x > -this.MAX_SPEED) {
-                my.sprite.player.setAccelerationX(-this.ACCELERATION);
-            } else {
-                my.sprite.player.setAccelerationX(0);
-            }
-            my.sprite.player.setFlip(true, false);
+        if(cursors.left.isDown) {
+            my.sprite.player.setAccelerationX(-this.ACCELERATION);
+            my.sprite.player.resetFlip();
             my.sprite.player.anims.play('walk', true);
             // particle following code here
             my.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth/2-10, my.sprite.player.displayHeight/2-5, false);
@@ -151,17 +136,11 @@ class Platformer extends Phaser.Scene {
 
                 my.vfx.walking.start();
 
-            } else {
-                my.vfx.walking.stop();
             }
 
-        } else if(!this.gameOver && cursors.right.isDown) {
-            if (my.sprite.player.body.velocity.x < this.MAX_SPEED) {
-                my.sprite.player.setAccelerationX(this.ACCELERATION);
-            } else {
-                my.sprite.player.setAccelerationX(0);
-            }
-            my.sprite.player.resetFlip();
+        } else if(cursors.right.isDown) {
+            my.sprite.player.setAccelerationX(this.ACCELERATION);
+            my.sprite.player.setFlip(true, false);
             my.sprite.player.anims.play('walk', true);
             // particle following code here
             my.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth/2-10, my.sprite.player.displayHeight/2-5, false);
@@ -174,8 +153,6 @@ class Platformer extends Phaser.Scene {
 
                 my.vfx.walking.start();
 
-            } else {
-                my.vfx.walking.stop();
             }
         } else {
             // Set acceleration to 0 and have DRAG take over
@@ -189,27 +166,11 @@ class Platformer extends Phaser.Scene {
 
         // player jump
         // note that we need body.blocked rather than body.touching b/c the former applies to tilemap tiles and the latter to the "ground"
-        if(!this.gameOver && my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
-            my.sprite.player.body.setVelocityY(this.JUMP_VELOCITY);
-            this.sound.play("jump", {
-                volume: 0.5
-            });
-            this.airborne = true;
-        }
-        
         if(!my.sprite.player.body.blocked.down) {
             my.sprite.player.anims.play('jump');
-            this.airborne = true;
-            this.timer--;
-        } else if (this.airborne == true && this.timer <= 0) {
-            this.sound.play("land");
-            this.airborne = false;
-            this.timer = 2;
         }
-        
-        if (!this.displayed && this.gameOver) {
-            my.text = this.add.text(this.cameras.main.worldView.centerX,this.cameras.main.worldView.centerY+50, "You win!\nScore: "+this.score)
-            this.displayed = true;
+        if(my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+            my.sprite.player.body.setVelocityY(this.JUMP_VELOCITY);
         }
 
         if(Phaser.Input.Keyboard.JustDown(this.rKey)) {
